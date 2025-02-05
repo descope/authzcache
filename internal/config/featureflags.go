5a2eb7b24799cf28085945e033c4ceb4eb3dc27d
Yosi Haran <yosi.haran@descope.com>
2025-02-05 17:39:52 +0200

    refactor: removed unused feature flag

--- internal/config/featureflags.go
+++ internal/config/featureflags.go
@@ -1,6 +1,3 @@
 package config
 
-import "github.com/descope/common/pkg/common/featureflags"
-
 // Feature flags
-var UpgradeSchemaForFGAFeature = &featureflags.FeatureFlag{Flag: "UPGRADE_SCHEMA_FOR_FGA", DefaultVal: true}
