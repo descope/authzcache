012689f4e513b3c1afb1bc87245198ee729319dd
Yosi Haran <yosi.haran@descope.com>
2026-05-27 19:33:01 +0300

    fix(lookups): fix wrongful immediate rejection of lookup candidates that are outside the Check cache (#566)

--- internal/services/authz_test.go
+++ internal/services/authz_test.go
@@ -336,6 +336,7 @@ func TestWhoCanAccess_CacheHitWithCandidateFiltering(t *testing.T) {
 	checkCallCount := 0
 	mockCache.CheckRelationFunc = func(_ context.Context, r *descope.FGARelation) (allowed bool, direct bool, ok bool) {
 		checkCallCount++
+		require.Equal(t, "*", r.TargetType)
 		if r.Target == "user2" {
 			return false, true, true
 		}
@@ -404,6 +405,7 @@ func TestWhatCanTargetAccess_CacheHitWithCandidateFiltering(t *testing.T) {
 	checkCallCount := 0
 	mockCache.CheckRelationFunc = func(_ context.Context, r *descope.FGARelation) (allowed bool, direct bool, ok bool) {
 		checkCallCount++
+		require.Equal(t, "*", r.TargetType)
 		if r.Resource == "doc2" {
 			return false, true, true
 		}
